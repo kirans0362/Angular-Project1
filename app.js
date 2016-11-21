abcb978c74e367b011ea172a90fc7023c8e2e194
Kiranmai <kiranmai@Kiranmais-MacBook-Pro.local>
2016-11-21 14:57:32 -0800

    Second commit , etc.. services and dependency injection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var myApp = angular.module("myApp",[]);
+var myApp = angular.module("myApp",["ngMessages","ngResource"]);
 
-myApp.controller("mainController", function($scope){
+myApp.controller("mainController", ["$scope","$log","$filter","$resource",function($scope,$log,$filter,$resource){
 	$scope.name = "kiranmai";
-	console.log($scope)
+	console.log($scope);
+	$log.log($scope.name);
+	$log.info($scope.name);
+	$scope.formattedname = $filter('uppercase')($scope.name);
+	$log.info($scope.formattedname);
 
-});
+
+}]);
